cd07fbe9ea9d4a2f5bf4408e74c207f0808ca481
Altamash-Sayyed <altamashsayyed786313@gmail.com>
2022-08-10 22:45:31 +0530

    make it better

--- index.js
+++ index.js
@@ -12,6 +12,12 @@ app.use(cors())
 app.use('/api/user',require('./routes/User.js'))
 app.use('/api/user',require('./routes/Auth.js'))
 
-
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("frontend/build"));
+    const path = require("path");
+    app.get("*", (req, res) => {
+      res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+    });
+  }
 
 app.listen(process.env.PORT|| 8000,()=>console.log('app is listen on http://localhost:8000'))
